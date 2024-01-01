13705934fea2422c41b05b5fe3b5ee462f78f41b
hakan.basturknewinn.com <hakan.basturk@newinn.com>
2024-01-01 21:46:20 +0300

    finished the test

--- ticketing/auth/src/routes/__test__/signout.test.ts
+++ ticketing/auth/src/routes/__test__/signout.test.ts
@@ -8,13 +8,13 @@ it('clear the cookie after signin out', async () => {
             email: 'test@test.com',
             password: 'password'
         })
-        .expect(201); 
+        .expect(201);
 
     const response = await request(app)
         .post('/api/users/signout')
         .send({})
         .expect(200);
-    
-    console.log(response.get('Set-Cookie'));
+
+    //console.log(response.get('Set-Cookie'));
     expect(response.get('Set-Cookie')[0]).toEqual('session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly');
 });
