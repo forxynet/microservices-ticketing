2ca6a0454e2995c862046266047372bcdd090770
Hakan Baştürk <forxynet@gmail.com>
2024-01-09 16:46:57 +0300

    updated tickets update test cases

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,10 +4,16 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@hakan.basturk/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
+
 app.set('trust proxy', true);
+
 app.use(json());
+
 app.use(
 	cookieSession({
 		signed: false,
@@ -18,6 +24,9 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
 	throw new NotFoundError();
